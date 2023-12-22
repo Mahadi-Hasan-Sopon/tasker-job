5223b4c4441d1d2f9328e479672854bdc7f4748e
Mahadi Hasan <mahadih137@gmail.com>
2023-12-22 13:33:52 +0600

    dashboard sidebar created

---
+++ src/components/DashboardSidebar.jsx
@@ -0,0 +1,68 @@
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/Logo.svg";
+import { Divider } from "antd";
+import {
+  AppstoreOutlined,
+  HomeOutlined,
+  PoweroffOutlined,
+} from "@ant-design/icons";
+
+const DashboardSidebar = () => {
+  return (
+    <div className="dashboard-sidebar flex flex-col gap-4 justify-center list-none h-full">
+      <Link to="/" className="logo flex items-center gap-2 cursor-pointer">
+        <img src={Logo} alt="" className="w-10 h-10 block mr-2" />
+        <h1 className="block text-2xl sm:text-3xl font-bold ">Tasker</h1>
+      </Link>
+      <Divider className="my-2" />
+
+      <li>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "font-medium bg-slate-100 text-blue-500 w-full" : ""
+          }
+          to="/dashboard"
+        >
+          <div className="flex items-center justify-start gap-2 py-2 px-3 rounded-md hover:text-white hover:bg-blue-500">
+            <AppstoreOutlined className="text-xl block -mt-1" />
+            <p className="font-medium text-lg">Dashboard</p>
+          </div>
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "font-medium bg-slate-100 text-blue-500"
+              : ""
+          }
+          to="/"
+        >
+          <div className="flex items-center justify-start gap-2 py-2 px-3 rounded-md hover:text-white hover:bg-blue-500">
+            <HomeOutlined className="text-xl block -mt-1" />
+            <p className="font-medium text-lg">Home</p>
+          </div>
+        </NavLink>
+      </li>
+      <div className="flex-grow"></div>
+
+      <div className="logout flex justify-center w-full">
+        <button
+          onClick={""}
+          type="button"
+          className="opacity-100 font-medium text-red-400 w-full border border-red-300 rounded-md"
+        >
+          <div className="flex items-center justify-center gap-2 py-2 px-5 rounded-md hover:text-white hover:bg-blue-500">
+            <PoweroffOutlined className="text-xl block -mt-1" />
+            <p className="font-medium text-lg">LogOut</p>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardSidebar;
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,11 +1,44 @@
 import { Outlet } from "react-router-dom";
+import DashboardSidebar from "../components/DashboardSidebar";
+import useAuth from "../hooks/useAuth";
+import { Divider } from "antd";
 
 const DashboardLayout = () => {
+  const { user } = useAuth();
+
+  const handleImageError = (e) => {
+    e.target.src = "https://i.ibb.co/ZXScD70/avatar.png";
+  };
+
   return (
     <div>
       <div className="grid grid-cols-12">
-        <div className="col-span-2">{/* <Sidebar /> */}</div>
-        <div className="col-span-10">
+        <div className="col-span-2 bg-amber-50/50 h-screen px-4 py-6 border-r shadow-lg">
+          {/* <Sidebar /> */}
+          <DashboardSidebar />
+        </div>
+        <div className="col-span-10 px-4">
+          {/* <TopBar /> */}
+          <div className="flex justify-end px-4 py-5">
+            <h1 className="text-3xl font-bold">
+              <div className="avatar flex justify-center items-center gap-4">
+                <div className="w-10 h-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img
+                    className="w-full h-full rounded-full"
+                    src={user?.photoURL}
+                    onError={handleImageError}
+                  />
+                </div>
+                <h3 className="text-sm font-semibold mt-2">
+                  {user?.displayName?.length > 12
+                    ? user?.displayName.slice(0, 12) + "..."
+                    : user?.displayName}
+                </h3>
+              </div>
+            </h1>
+          </div>
+          <Divider className="my-2" />
+          {/* <Outlet /> */}
           <Outlet />
         </div>
       </div>
